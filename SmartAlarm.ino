f85d0d22228b477304266601ebcad69b7c5faf52
tamilsm <tsmur4@student.monash.edu>
2019-09-29 16:19:52 +1000

    Screen code updated
    The screen works with less brightness and touch has been disabled

--- SmartAlarm.ino
+++ SmartAlarm.ino
@@ -1,9 +1,3 @@
-// the regular Adafruit "TouchScreen.h" library only works on AVRs
-
-// different mcufriend shields have Touchscreen on different pins
-// and rotation.
-// Run the UTouch_calibr_kbv sketch for calibration of your shield
-
 #include <Adafruit_GFX.h>    // Core graphics library
 //#include <Adafruit_TFTLCD.h> // Hardware-specific library
 //Adafruit_TFTLCD tft(A3, A2, A1, A0, A4);
@@ -17,23 +11,23 @@ MCUFRIEND_kbv tft;       // hard-wired for UNO shields anyway.
 #endif
 
 // most mcufriend shields use these pins and Portrait mode:
-uint8_t YP = A1;  // must be an analog pin, use "An" notation!
-uint8_t XM = A2;  // must be an analog pin, use "An" notation!
-uint8_t YM = 7;   // can be a digital pin
-uint8_t XP = 6;   // can be a digital pin
-uint8_t SwapXY = 0;
+//uint8_t YP = A1;  // must be an analog pin, use "An" notation!
+//uint8_t XM = A0;  // must be an analog pin, use "An" notation!
+//uint8_t YM = 7;   // can be a digital pin
+//uint8_t XP = 6;   // can be a digital pin
+//uint8_t SwapXY = 0;
 
-uint16_t TS_LEFT = 920;
-uint16_t TS_RT  = 150;
-uint16_t TS_TOP = 940;
-uint16_t TS_BOT = 120;
+uint16_t TS_LEFT = 893;
+uint16_t TS_RT  = 208;
+uint16_t TS_TOP = 900;
+uint16_t TS_BOT = 340;
 char *name = "Unknown controller";
 
 // For better pressure precision, we need to know the resistance
 // between X+ and X- Use any multimeter to read it
 // For the one we're using, its 300 ohms across the X plate
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
-TSPoint tp;
+//TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+//TSPoint tp;
 
 #define MINPRESSURE 20
 #define MAXPRESSURE 850
@@ -67,109 +61,42 @@ void setup(void)
     identifier = 0;
     
     name = "ILI9341 DealExtreme";
-    TS_LEFT = 893; TS_RT = 208; TS_TOP = 900; TS_BOT = 340;
-    SwapXY = 1;
-    switch (Orientation) {      // adjust for different aspects
-        case 0:   break;        //no change,  calibrated for PORTRAIT
-        case 1:   tmp = TS_LEFT, TS_LEFT = TS_BOT, TS_BOT = TS_RT, TS_RT = TS_TOP, TS_TOP = tmp;  break;
-        case 2:   SWAP(TS_LEFT, TS_RT);  SWAP(TS_TOP, TS_BOT); break;
-        case 3:   tmp = TS_LEFT, TS_LEFT = TS_TOP, TS_TOP = TS_RT, TS_RT = TS_BOT, TS_BOT = tmp;  break;
-    }
-
-    Serial.begin(9600);
-    ts = TouchScreen(XP, YP, XM, YM, 300);     //call the constructor AGAIN with new values.
+//    SwapXY = 1;
+//    switch (Orientation) {      // adjust for different aspects
+//        case 0:   break;        //no change,  calibrated for PORTRAIT
+//        case 1:   tmp = TS_LEFT, TS_LEFT = TS_BOT, TS_BOT = TS_RT, TS_RT = TS_TOP, TS_TOP = tmp;  break;
+//        case 2:   SWAP(TS_LEFT, TS_RT);  SWAP(TS_TOP, TS_BOT); break;
+//        case 3:   tmp = TS_LEFT, TS_LEFT = TS_TOP, TS_TOP = TS_RT, TS_RT = TS_BOT, TS_BOT = tmp;  break;
+//    }
+//
+//    Serial.begin(9600);
+//    ts = TouchScreen(XP, YP, XM, YM, 300);     //call the constructor AGAIN with new values.
     tft.begin(identifier);
-//    show_Serial();
     tft.setRotation(Orientation);
     tft.fillScreen(BLACK);
-//    show_tft();
-
-    BOXSIZE = tft.width() / 6;
-    tft.fillScreen(BLACK);
-
-    tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
-    tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
-    tft.fillRect(BOXSIZE * 2, 0, BOXSIZE, BOXSIZE, GREEN);
-    tft.fillRect(BOXSIZE * 3, 0, BOXSIZE, BOXSIZE, CYAN);
-    tft.fillRect(BOXSIZE * 4, 0, BOXSIZE, BOXSIZE, BLUE);
-    tft.fillRect(BOXSIZE * 5, 0, BOXSIZE, BOXSIZE, MAGENTA);
-
-    tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
-    currentcolor = RED;
-    delay(1000);
+    tft.println(" COLOR GRADES");
 }
 
 void loop()
 {
     uint16_t xpos, ypos;  //screen coordinates
-    tp = ts.getPoint();   //tp.x, tp.y are ADC values
-//    Serial.println("Over here");
+//    tp = ts.getPoint();   //tp.x, tp.y are ADC values
+
     // if sharing pins, you'll need to fix the directions of the touchscreen pins
-    pinMode(XM, OUTPUT);
-    pinMode(YP, OUTPUT);
-    pinMode(XP, OUTPUT);
-    pinMode(YM, OUTPUT);
-    //    digitalWrite(XM, HIGH);
-    //    digitalWrite(YP, HIGH);
-    // we have some minimum pressure we consider 'valid'
-    // pressure of 0 means no pressing!
+//    pinMode(XM, OUTPUT);
+//    pinMode(YP, OUTPUT);
+//    pinMode(XP, OUTPUT);
+//    pinMode(YM, OUTPUT);
 
-    if (tp.z > MINPRESSURE && tp.z < MAXPRESSURE) {
-        // is controller wired for Landscape ? or are we oriented in Landscape?
-        if (SwapXY != (Orientation & 1)) SWAP(tp.x, tp.y);
-        // scale from 0->1023 to tft.width  i.e. left = 0, rt = width
-        // most mcufriend have touch (with icons) that extends below the TFT
-        // screens without icons need to reserve a space for "erase"
-        // scale the ADC values from ts.getPoint() to screen values e.g. 0-239
-        xpos = map(tp.x, TS_LEFT, TS_RT, 0, tft.width());
-        ypos = map(tp.y, TS_TOP, TS_BOT, 0, tft.height());
-
-        
-       Serial.print("X = "); Serial.print(tp.x);
-       Serial.print("\tY = "); Serial.print(tp.y);
-       Serial.print("\tPressure = "); Serial.println(tp.z);
-
-        // are we in top color box area ?
-        if (ypos < BOXSIZE) {               //draw white border on selected color box
-            oldcolor = currentcolor;
-
-            if (xpos < BOXSIZE) {
-                currentcolor = RED;
-                tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
-            } else if (xpos < BOXSIZE * 2) {
-                currentcolor = YELLOW;
-                tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, WHITE);
-            } else if (xpos < BOXSIZE * 3) {
-                currentcolor = GREEN;
-                tft.drawRect(BOXSIZE * 2, 0, BOXSIZE, BOXSIZE, WHITE);
-            } else if (xpos < BOXSIZE * 4) {
-                currentcolor = CYAN;
-                tft.drawRect(BOXSIZE * 3, 0, BOXSIZE, BOXSIZE, WHITE);
-            } else if (xpos < BOXSIZE * 5) {
-                currentcolor = BLUE;
-                tft.drawRect(BOXSIZE * 4, 0, BOXSIZE, BOXSIZE, WHITE);
-            } else if (xpos < BOXSIZE * 6) {
-                currentcolor = MAGENTA;
-                tft.drawRect(BOXSIZE * 5, 0, BOXSIZE, BOXSIZE, WHITE);
-            }
-
-            if (oldcolor != currentcolor) { //rub out the previous white border
-                if (oldcolor == RED) tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
-                if (oldcolor == YELLOW) tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
-                if (oldcolor == GREEN) tft.fillRect(BOXSIZE * 2, 0, BOXSIZE, BOXSIZE, GREEN);
-                if (oldcolor == CYAN) tft.fillRect(BOXSIZE * 3, 0, BOXSIZE, BOXSIZE, CYAN);
-                if (oldcolor == BLUE) tft.fillRect(BOXSIZE * 4, 0, BOXSIZE, BOXSIZE, BLUE);
-                if (oldcolor == MAGENTA) tft.fillRect(BOXSIZE * 5, 0, BOXSIZE, BOXSIZE, MAGENTA);
-            }
-        }
-        // are we in drawing area ?
-        if (((ypos - PENRADIUS) > BOXSIZE) && ((ypos + PENRADIUS) < tft.height())) {
-            tft.fillCircle(xpos, ypos, PENRADIUS, currentcolor);
-        }
-        // are we in erase area ?
-        if (ypos > tft.height() - 10) {
-            // press the bottom of the screen to erase
-            tft.fillRect(0, BOXSIZE, tft.width(), tft.height() - BOXSIZE, BLACK);
-        }
-    }
+//    if (tp.z > MINPRESSURE && tp.z < MAXPRESSURE) {
+//        // is controller wired for Landscape ? or are we oriented in Landscape?
+//        if (SwapXY != (Orientation & 1)) SWAP(tp.x, tp.y);
+//        xpos = map(tp.x, TS_LEFT, TS_RT, 0, tft.width());
+//        ypos = map(tp.y, TS_TOP, TS_BOT, 0, tft.height());
+//
+//        
+//       Serial.print("X = "); Serial.print(tp.x);
+//       Serial.print("\tY = "); Serial.print(tp.y);
+//       Serial.print("\tPressure = "); Serial.println(tp.z);
+//    }
 }
